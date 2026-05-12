e58978d4a91fc3e2cc529c76b420d0423c5392e4
Brendah Namubali <160923755+BrendahNamubali@users.noreply.github.com>
2026-05-12 14:03:24 +0300

    Create DemoCasesPage.tsx

---
+++ src/pages/DemoCasesPage.tsx
@@ -0,0 +1,8 @@
+export default function DemoCasesPage() {
+  return (
+    <div style={{ padding: "40px" }}>
+      <h1>Demo Cases</h1>
+      <p>LunaDX demo cases coming soon.</p>
+    </div>
+  );
+}
