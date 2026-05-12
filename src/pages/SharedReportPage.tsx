13914e226f518cdabcafac80d37c9c610da63489
Brendah Namubali <160923755+BrendahNamubali@users.noreply.github.com>
2026-05-12 14:15:33 +0300

    Create SharedReportPage.tsx

---
+++ src/pages/SharedReportPage.tsx
@@ -0,0 +1,8 @@
+export default function SharedReportPage() {
+  return (
+    <div style={{ padding: "40px" }}>
+      <h1>Shared Report</h1>
+      <p>Report sharing page coming soon.</p>
+    </div>
+  );
+}
